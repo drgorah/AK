23f31a20cc2fd8973c5ca1e993634ca427a17bb7
ak <ak@thusspakeak.com>
2019-01-15 11:27:01 +0000

    fixing copyright notice

--- Cluster/RawClustering.js
+++ Cluster/RawClustering.js
@@ -1,6 +1,6 @@
 //AK/Cluster/RawClustering.js
 
-//Copyright Richard Harris 2015.
+//Copyright Richard Harris 2018.
 //Distributed under the Boost Software License, Version 1.0.
 //(See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
