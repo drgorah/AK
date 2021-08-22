7efe19969d7e44b4dc473ddeb22a4529666193f0
ak <ak@thusspakeak.com>
2021-08-22 10:03:43 +0100

    reversing order of xors

--- Number/GrayCode.js
+++ Number/GrayCode.js
@@ -17,11 +17,11 @@
   };
 
   ak.fromGray = function(i) {
-   i ^= i >>> 16;
-   i ^= i >>> 8;
-   i ^= i >>> 4;
+   i ^= i >>> 1;
    i ^= i >>> 2;
-   return i ^ (i >>> 1);
+   i ^= i >>> 4;
+   i ^= i >>> 8;
+   return i ^ (i >>> 16);
   };
  }
 
